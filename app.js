1385dc725fd0986e71f6149b13bbe0b08f522d66
marielcastro <marielfx@gmail.com>
2024-02-01 22:50:45 -0600

    Agregando numeros telefonicos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let datos = ["Juan","Billy","Kevin","Karenci","Melly","Juina"];
-
+let telefonos = ["93842002","38470211","39482031","93842002","38470211","39482031"];
 datos.forEach(valor=>{
     console.log(valor);
 })
